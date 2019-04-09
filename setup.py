835c0671fc6e4e46d06e71a23b1c70be6fdd240f
EricP <eric.parker@mentice.com>
2019-04-09 13:16:27 -0500

    MANIFEST.in, DESCRIPTION.md

--- setup.py
+++ setup.py
@@ -7,12 +7,17 @@
 
 from setuptools import find_packages, setup
 
+with open('DESCRIPTION.md', 'r') as inf:
+	DESCRIPTION = inf.read()
+
 setup(
 	name = 'docker-jobber',
 	version='0.3.0',		# NOTE Keep synced with __version__ in jobber/__init__.py
 	author="Eric Parker",
 	author_email="eric.parker@mentice.com",
 	description="A command line interface (CLI) application for managing machine learning workflows using Docker",
+	long_description=DESCRIPTION,
+	long_description_content_type='text/markdown',
 	url="https://github.com/mentice/docker-jobber",
 	classifiers=[
 		"Programming Language :: Python :: 3",
@@ -22,11 +27,11 @@ setup(
 	],
 	packages=find_packages('src'),
 	package_dir={'':'src'},
-	install_requires=['PyYAML', 'async_timeout', 'jsonmerge', 'click', 'docker'],
+	include_package_data=True,
+	install_requires=['PyYAML', 'async-timeout', 'jsonmerge', 'click', 'docker'],
 	entry_points = {
 		'console_scripts': [
 			'jobber=cli.cli:cli',
 		],
 	},
-	zip_safe=False,
 )
